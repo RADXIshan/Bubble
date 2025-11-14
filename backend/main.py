d7d3092e92dc8bd4e4a868478239bf5e9e41c45b
Ishan Roy <ishanroy3118107@gmail.com>
2025-11-15 02:16:11 +0530

    feat(api): add health check endpoint
    Add new /heath endpoint to monitor server status. Also update render.yaml build and start commands to include backend directory and change client URL.

--- backend/main.py
+++ backend/main.py
@@ -34,5 +34,10 @@ def server_live():
     return ServerStatus(status="Success", message="Server is live!")
 
 
+@app.get("/heath", response_model=ServerStatus, status_code=200)
+def server_health():
+    return ServerStatus(status="Healthy", message="Server is working perfectly")
+
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=PORT)
